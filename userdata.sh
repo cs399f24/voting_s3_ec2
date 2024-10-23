f9ecfba022cb1a30e1cc149373c69d0464ee3017
Ben Coleman <colemanb@moravian.edu>
2024-10-23 05:54:13 -0400

    add launch config

---
+++ userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum install -y git
+git clone https://github.com/cs399f24/voting_s3_ec2.git
+cd voting_s3_ec2
+python3 -m venv .venv
+.venv/bin/pip install -r requirements.txt
+yum install -y redis6
+systemctl start redis6
+echo "REDIS_HOST=localhost" >> .env
+echo "REDIS_PORT=6379" >> .env
+cp voting.service /etc/systemd/system
+systemctl enable voting
+systemctl start voting
